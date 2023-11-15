2028d791dc6380c832b3cfa1247cd2b05dea8e15
Your Name <you@example.com>
2023-11-15 02:32:43 -0300

    first commit

---
+++ ordenacao.py
@@ -0,0 +1,8 @@
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = int(input("Digite o terceiro valor: "))
+
+if (a <= b) and (b <= c):
+	print("crescente")
+else:
+	print("não está em ordem crescente")
